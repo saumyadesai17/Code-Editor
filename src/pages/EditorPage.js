1df3f71ba481116add1e65b6fb3891b0984410c7
konduskarsuyash <suyashkonduskar27@gmail.com>
2023-12-01 22:46:59 +0530

    Till ws-error handling but right now the backend nodemon is crashing

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,13 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState,useRef, useEffect } from 'react'
+import toast from 'react-hot-toast';
 import Client from '../components/Client';
 import Editor  from '../components/Editor';
+import { initSocket } from '../socket';
+import { useLocation,useNavigate,Navigate,useParams } from 'react-router-dom';
+// import {Actions} from '../Actions.js'
 
 
 export default function EditorPage() {
+    const socketRef = useRef(null);
+    const location = useLocation();
+    const reactNavigator = useNavigate();
+    const {roomId} = useParams();
+
+    useEffect(() =>{
+        const init = async () =>{
+            socketRef.current = await initSocket();
+            socketRef.current.on('connect_error', (err) => handleErrors(err));
+
+            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+            function handleErrors(e) {
+                console.log('socket error', e);
+                toast.error('Socket connection failed, try again later.');
+                reactNavigator('/');
+            }
+
+            socketRef.current.emit(ACTIONS.JOIN,{
+                roomId,
+                username:location.state?.username,
+
+            });
+
+
+            //listening for joied event
+            socketRef.current.on(ACTIONS.JOINED, ({clients,username,socketId}) =>{
+                if(username !== location.state?.username){
+                    toast.success(`${username} joined the room.`)
+                }
+                setClients(clients)
+
+            })
+
+        };
+        init();
+
+    },[]);
+
+
     const [clients,setClients]=useState([
-        {socketId:1,username:'Suyash'},{socketId:2,username:'Saumya'},
-        {socketId:3,username:'Vivek'}
+        // {socketId:1,username:'Suyash'},{socketId:2,username:'Saumya'},
+        // {socketId:3,username:'Vivek'}
 ]);
+
+    if(!location.state){
+        return <Navigate to="/"/>
+    }
   return (
     <div className='mainWrap'>
         <div className="aside">
